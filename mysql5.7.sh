c4ecadf2fa3801fdecf11387c29ec00c8983cf68
Md. Masud Parvaze <141165442+parvaze-masud@users.noreply.github.com>
2023-10-14 22:56:16 +0600

    Update mysql5.7.sh

--- mysql5.7.sh
+++ mysql5.7.sh
@@ -10,6 +10,25 @@
 
 [mas@DB-node yum.repos.d]$ sudo vim mysql-community.repo
 
+########################################################################
+# Enable to use MySQL 5.7
+[mysql57-community]
+name=MySQL 5.7 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch
+enabled=1
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql-2022
+       file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+
+[mysql80-community]
+name=MySQL 8.0 Community Server
+baseurl=http://repo.mysql.com/yum/mysql-8.0-community/el/7/$basearch
+enabled=0
+gpgcheck=1
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql-2022
+       file:///etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
+###############################################################################3
+
 [mas@DB-node yum.repos.d]$ yum repolist
 
 [mas@DB-node yum.repos.d]$ sudo yum install mysql-community-server
