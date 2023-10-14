2eee057b93d812f29c93e3edcdf1a48a18c5796c
Md. Masud Parvaze <141165442+parvaze-masud@users.noreply.github.com>
2023-10-14 12:13:24 +0600

    Create mysql5.7.sh

---
+++ mysql5.7.sh
@@ -0,0 +1,37 @@
+#### CentOs 7#####
+
+[mas@DB-node ~]$ wget https://dev.mysql.com/get/mysql80-community-release-el7-10.noarch.rpm
+
+[mas@DB-node ~]$ sudo yum localinstall mysql80-community-release-el7-10.noarch.rpm
+
+[mas@DB-node ~]$ sudo yum repolist enabled | grep "mysql.*-community.*"
+
+[mas@DB-node yum.repos.d]$ cd /etc/yum.repos.d/
+
+[mas@DB-node yum.repos.d]$ sudo vim mysql-community.repo
+
+[mas@DB-node yum.repos.d]$ yum repolist
+
+[mas@DB-node yum.repos.d]$ sudo yum install mysql-community-server
+
+[mas@DB-node yum.repos.d]$ sudo systemctl enable mysqld
+
+[mas@DB-node yum.repos.d]$ sudo systemctl start mysqld
+
+[mas@DB-node yum.repos.d]$ mysql -V
+
+[mas@DB-node yum.repos.d]$ sudo grep "A temporary password" /var/log/mysqld.log | tail
+t*&%rhEw#6dt
+
+[mas@DB-node yum.repos.d]$ sudo mysql_secure_installation
+Password: PRG@1qaz (root)
+
+[mas@DB-node yum.repos.d]$ mysql -u root -p
+
+mysql> CREATE DATABASE forum;
+
+mysql> CREATE USER 'forum'@'%' IDENTIFIED BY 'PRG@1qaz';
+
+mysql> GRANT ALL PRIVILEGES ON forum.* TO 'forum'@'%';
+
+mysql> FLUSH PRIVILEGES;
